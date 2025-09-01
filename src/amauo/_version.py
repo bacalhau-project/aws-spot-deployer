2ba94e7470b885b30922083c512165f3eb5ea6b5
David Aronchick <aronchick@gmail.com>
2025-08-31 17:57:23 -0700

    adding version

--- src/amauo/_version.py
+++ src/amauo/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '3.0.8.dev1'
-__version_tuple__ = version_tuple = (3, 0, 8, 'dev1')
+__version__ = version = '3.0.8.dev2'
+__version_tuple__ = version_tuple = (3, 0, 8, 'dev2')
 
 __commit_id__ = commit_id = None
